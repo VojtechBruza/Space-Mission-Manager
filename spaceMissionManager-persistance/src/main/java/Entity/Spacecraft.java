76703eeae21e1905c5c6c71ce3dd3bc72afedc7d
VojtechBruza <vojtech.bruza@gmail.com>
2017-10-21 12:14:07 +0200

    Basic Spacecraft entity for dao impl

--- spaceMissionManager-persistance/src/main/java/Entity/Spacecraft.java
+++ spaceMissionManager-persistance/src/main/java/Entity/Spacecraft.java
@@ -1,10 +1,67 @@
 package Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
 public class Spacecraft {
 
-	private int unfinishedComponents;
-	private String name;
-	private String type;
+	@Id
 	private Long id;
 
+	private String type; //	Probes vs astronauts? type not needed
+
+	private String name;
+	@OneToMany
+	private List<CraftComponent> allComponents;
+
+//	private int unfinishedComponents; //rather use method?
+
+
+	public Spacecraft(List<CraftComponent> allComponents) {
+		this.allComponents = allComponents;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<CraftComponent> getAllComponents() {
+		return allComponents;
+	}
+
+	public int unfinishedComponentsCount(){
+		int count = allComponents.size();
+		for (CraftComponent c : allComponents){
+			if(c.isReadyToUse()){
+				count--;
+			}
+		}
+		return count;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Spacecraft)) return false;
+		Spacecraft spacecraft = (Spacecraft) o;
+		return getId() != null ? getId().equals(spacecraft.getId()) : spacecraft.getId() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return getId() != null ? getId().hashCode() : 0;
+	}
 }
